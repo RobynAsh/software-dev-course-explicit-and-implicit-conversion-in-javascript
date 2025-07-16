b9a53db1e0986c86125bb6f7f063eceececba4d1
RobynAsh <rnashauer@gmail.com>
2025-07-15 22:35:29 -0500

    Assignment completed

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,39 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
+//Part 1
+
+let result = Number("5") - 2; // Explicitly changes "5" to a number so the result is always a number
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let isValid = Boolean("false"); //Already explicit. Not sure what is needed here? The code will run exactly as expected and give an output of "This is Valid" It has already made a string and explicit boolean and because the string is not empty is a truthy
 if (isValid) {
-    console.log("This is valid!");
+  console.log("This is valid!");
 }
 
-let age = "25";
+let age = Number("25"); //Explicitly changes "25" string to a number so the results of totalAge are able to perform as intended and produce 30 instead of 255
 let totalAge = age + 5;
 console.log("Total Age: " + totalAge);
+
+// Part 2
+
+// Implicit
+
+let apples = 5;
+let fruitType = "apples"
+console.log("Before: 'apples' is a number with value: " + apples);
+
+let combined = "There are " + apples + " " + fruitType + ".";
+console.log("After: 'combined' is a string with a value in it: " + combined);
+
+//The code implicitly know to combine the number and the string together in one string. I don't need to tell it. 
+
+// Explicit
+
+let wordText = "hello";
+console.log("What we start with (text): " + wordText);
+
+let changedToNumber = Number(wordText);
+console.log("What it becomes when we try to make it a number: " + changedToNumber)
+
+//The code explicitly tells it to convert from a string to a number. The string is a word so it can not convert and results in NaN. 
